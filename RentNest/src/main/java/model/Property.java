a1ed13b5a37921a7196ca5758cc4ee61b482836f
Bibek Kumar Sah <shahbibek181@gmail.com>
2026-05-07 20:27:50 +0545

    Added property model

--- RentNest/src/main/java/model/Property.java
+++ RentNest/src/main/java/model/Property.java
@@ -1,103 +1,104 @@
 package model;
 
+import java.sql.Timestamp;
+
 public class Property {
-	private int propertyId;
-	private int ownerId;
-	private String title;
-	private String type;
-	private String location;
-	private double price;
-	private int rooms;
-	private String description;
-	private String image;
-	private String status;
-	private String availability;
-
-	public int getOwnerId() {
-		return ownerId;
-	}
-
-	public void setOwnerId(int ownerId) {
-		this.ownerId = ownerId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public int getRooms() {
-		return rooms;
-	}
-
-	public void setRooms(int rooms) {
-		this.rooms = rooms;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getAvailability() {
-		return availability;
-	}
-
-	public void setAvailability(String availability) {
-		this.availability = availability;
-	}
-
-	public int getPropertyId() {
-		return propertyId;
-	}
-
-	public void setPropertyId(int propertyId) {
-		this.propertyId = propertyId;
-	}
-}
+	 private int propertyId;
+	    private String title;
+	    private String type;
+	    private String location;
+	    private double price;
+	    private int bedrooms;
+	    private int bathrooms;
+	    private int areaSqft;
+	    private String description;
+	    private String image;
+	    private boolean availability;
+	    private boolean status;
+	    private boolean isDeleted;
+	    private Timestamp createdAt;
+		public int getPropertyId() {
+			return propertyId;
+		}
+		public void setPropertyId(int propertyId) {
+			this.propertyId = propertyId;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getType() {
+			return type;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
+		public String getLocation() {
+			return location;
+		}
+		public void setLocation(String location) {
+			this.location = location;
+		}
+		public double getPrice() {
+			return price;
+		}
+		public void setPrice(double price) {
+			this.price = price;
+		}
+		public int getBedrooms() {
+			return bedrooms;
+		}
+		public void setBedrooms(int bedrooms) {
+			this.bedrooms = bedrooms;
+		}
+		public int getBathrooms() {
+			return bathrooms;
+		}
+		public void setBathrooms(int bathrooms) {
+			this.bathrooms = bathrooms;
+		}
+		public int getAreaSqft() {
+			return areaSqft;
+		}
+		public void setAreaSqft(int areaSqft) {
+			this.areaSqft = areaSqft;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public String getImage() {
+			return image;
+		}
+		public void setImage(String image) {
+			this.image = image;
+		}
+		public boolean isAvailability() {
+			return availability;
+		}
+		public void setAvailability(boolean availability) {
+			this.availability = availability;
+		}
+		public boolean isStatus() {
+			return status;
+		}
+		public void setStatus(boolean status) {
+			this.status = status;
+		}
+		public boolean isDeleted() {
+			return isDeleted;
+		}
+		public void setDeleted(boolean isDeleted) {
+			this.isDeleted = isDeleted;
+		}
+		public Timestamp getCreatedAt() {
+			return createdAt;
+		}
+		public void setCreatedAt(Timestamp createdAt) {
+			this.createdAt = createdAt;
+		}
+	    }
